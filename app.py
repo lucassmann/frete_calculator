3552ecf5716718eb211e82542935b145f70cfa52
lucassmann <lucas.assmann@edu.pucrs.br>
2022-09-04 23:35:04 -0300

    Oferta.id como autoincrement pk, fretadora agora reflete o nome e html

--- app.py
+++ app.py
@@ -8,13 +8,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Oferta(db.Model):
-    # mudar pra Integer e auto increment quando souber como faz
-    id = db.Column(db.String(100), primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     # mudar Integer p decimal para representar dinheiro
     valor_km = db.Column(db.Integer)
     valor_carga_descarga = db.Column(db.Integer)
     # temporario! mudar para cod assim que usuarios de fretador/remetente/destinatario forem implementados
-    fretador = db.Column(db.String(100))
+    fretadora = db.Column(db.String(100))
     # temporario! mudar para ANTT_piso assim que for implementado com checagem de valores minimos
     categoria_transporte = db.Column(db.String(100))
     tipo_carga = db.Column(db.String(100))
@@ -40,14 +39,14 @@ def fretadora_oferta_cadastro_view():
 
 @app.post("/fretadora/ofertas/cadastro/criar")
 def oferta_post():
-    empresa = request.form.get("empresa")
+    fretadora = request.form.get("fretadora")
     categoria_transporte = request.form.get("categorias")
     tipo_carga = request.form.get("carga")
     eixos = request.form.get("eixos")
     valor_km = request.form.get("valor_km")
     valor_carga_descarga = request.form.get("valor_carga_desgarga")
     
-    new_oferta = Oferta(id=empresa, categoria_transporte=categoria_transporte, tipo_carga = tipo_carga, eixos = eixos, valor_km = valor_km, valor_carga_descarga = valor_carga_descarga)
+    new_oferta = Oferta(fretadora=fretadora, categoria_transporte=categoria_transporte, tipo_carga = tipo_carga, eixos = eixos, valor_km = valor_km, valor_carga_descarga = valor_carga_descarga)
     db.session.add(new_oferta)
     db.session.commit()
     return redirect(url_for("home"))
